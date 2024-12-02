8abb2e6eadae071545bdd985fb7299ed3e885a02
MerVitz <amakaluvitalis202@gmail.com>
2024-12-02 03:17:56 +0000

    fixed, neeeded to add a seperate file. In python modules referenced cannot start with a number

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -5,6 +5,7 @@ Module that defines an asynchronous comprehension.
 from typing import List
 from async_generator import async_generator
 
+
 async def async_comprehension() -> List[float]:
     """
     Collects 10 random numbers from async_generator using async comprehension.
